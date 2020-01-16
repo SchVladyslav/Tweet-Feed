5fc2d461238c90e6bcad171b7c386cb3283d183d
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-16 21:39:55 +0200

    Page hierarchy added

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,30 +1,5 @@
-import React, { Component } from "react";
-import "./SignInPage.scss";
-import AuthSignIn from "../../components/auth/AuthSignIn";
-import { Link } from "react-router-dom";
+import React from "react";
 
-class SignInPage extends Component {
-  render() {
-    return (
-      <div className="container">
-        <main className="main-content">
-          <div className="main-logo-page">
-            <i className="icon icon-mountains"></i>
-          </div>
-          <h1 className="main__title">Sign In</h1>
-
-          <AuthSignIn />
-
-          <div className="form__controls">
-            <p className="main__social-text">Or sign up using</p>
-            <Link className="hint" to="/signup">
-              Don't have an account?
-            </Link>
-          </div>
-        </main>
-      </div>
-    );
-  }
+export default function SignInPage() {
+  return <div></div>;
 }
-
-export default SignInPage;
