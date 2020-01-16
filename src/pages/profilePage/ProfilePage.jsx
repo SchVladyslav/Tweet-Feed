38e70cb5cc6e2870db8f5095ff9ef87539319a64
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-16 21:39:55 +0200

    Page hierarchy added

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
 
 export default function ProfilePage() {
-    return (
-        <Layout>
-          <h1>Profile Page!</h1>
-        </Layout>
-    )
+  return <div></div>;
 }
