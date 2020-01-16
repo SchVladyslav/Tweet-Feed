a6d4d8c27f2899e52cec3614be74e0fdbfb1df20
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-16 21:39:55 +0200

    Page hierarchy added

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,55 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
-import PropTypes from "prop-types";
 
-export const Button = (props, { buttonState }) => {
-  return (
-    <button
-      className={props.className}
-      type={props.type}
-      disabled={!buttonState}
-    >
-      {props.label}
-    </button>
-  );
-};
-
-Button.defaultProps = {
-  type: "button",
-  className: ""
-};
-
-Button.propTypes = {
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(["button", "submit"]),
-  className: PropTypes.string
-};
-=======
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-
-import "./SignUpPage.scss";
-
-import { AuthSignUp } from "../../components/auth/AuthSignUp";
-
-export default class SignUpPage extends Component {
-  render() {
-    return (
-      <div className="container">
-        <main className="main-content">
-          <div className="main-back">
-            <Link to="/signin">&larr;</Link>
-          </div>
-          <div className="main-logo-page">
-            <i className="icon icon-mountains"></i>
-          </div>
-          <h1 className="main__title">Sign Up</h1>
-          <AuthSignUp />
-        </main>
-      </div>
-    );
-  }
+export default function SignUpPage() {
+  return <div></div>;
 }
->>>>>>> Authorization added
