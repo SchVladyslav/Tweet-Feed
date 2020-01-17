f05eef6d129ed4871a7852c55c4fa89a5156a3de
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-17 13:57:23 +0200

    common elements added

--- src/pages/eventsPage/EventsPage.jsx
+++ src/pages/eventsPage/EventsPage.jsx
@@ -1,6 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export default function EventsPage() {
-  return <div></div>;
-}
+export const Input = props => {
+  const InputProps = {
+    className: props.className,
+    name: props.name,
+    type: props.type,
+    placeholder: props.placeholder,
+    value: props.value,
+    handleChange: props.handleChange,
+    label: props.label,
+    error: props.error
+  };
+
+  return (
+    <React.Fragment>
+      {/* <label htmlFor={props.name} className="input-label">
+        {InputProps.label}
+      </label> */}
+      <input
+        className={InputProps.className}
+        name={InputProps.name}
+        type={InputProps.type}
+        value={InputProps.value}
+        handleChange={InputProps.handleChange}
+        placeholder={InputProps.placeholder}
+        style={InputProps.error && { border: "1px solid red" }}
+      />
+      {InputProps.error && <p>{InputProps.error}</p>}
+    </React.Fragment>
+  );
+};
+
+Input.defaultProps = {
+  type: "text",
+  className: ""
+};
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  type: PropTypes.oneOf(["text", "number", "password"]),
+  placeholder: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired
+};
