27779045ba4f57587c781b56fb718d8a484bba23
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-17 13:57:23 +0200

    common elements added

--- src/components/elements/Form.jsx
+++ src/components/elements/Form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Input } from "../common/Input";
 import { Button } from "../common/Button";
+<<<<<<< HEAD
 import { ValidateFields } from "../../helpers/ValidateFields";
 
 export default class Form extends Component {
@@ -14,10 +15,20 @@ export default class Form extends Component {
     };
 
     this.state = {
+=======
+
+export default class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+>>>>>>> common elements added
       formValid: false
     };
   }
 
+<<<<<<< HEAD
   handleUserInput = e => {
     const { name } = e.target;
     const { value } = e.target;
@@ -82,11 +93,46 @@ export default class Form extends Component {
 {
   /* <Input
               className="input input__password"
+=======
+  render() {
+    return (
+      <form className="main__form" action="">
+        <ul className="form__list">
+          <li>
+            <Input
+              className="input__email"
+              type="email"
+              placeholder="E-mail"
+              name="email"
+              value={this.state.email}
+              handleChange={event => this.handleUserInput(event)}
+            />
+            <i className="icon icon-user input__icon"></i>
+          </li>
+          <li>
+            <Input
+              className="input__password"
+>>>>>>> common elements added
               type="password"
               placeholder="Password"
               name="password"
               value={this.state.password}
               handleChange={event => this.handleUserInput(event)}
             />
+<<<<<<< HEAD
             <i className="icon icon-lock input__icon"></i> */
+=======
+            <i className="icon icon-lock input__icon"></i>
+          </li>
+        </ul>
+        <Button
+          label="Sign Up"
+          className="button"
+          type="submit"
+          buttonState={this.state.formValid}
+        />
+      </form>
+    );
+  }
+>>>>>>> common elements added
 }
