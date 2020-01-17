86df0c0814b10955081ab843948b5fab7ebc7f72
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-17 13:57:23 +0200

    common elements added

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,7 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Auth extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+export const Auth = () => {
+  return <div></div>;
+};
