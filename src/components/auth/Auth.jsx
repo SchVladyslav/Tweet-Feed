0fc1fa39ad8a06d8c54d8c023161833a6c6274e3
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-20 10:51:00 +0200

    Authorization added

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 // import React, { Component } from "react";
 // import { withRouter } from "react-router-dom";
 
@@ -92,3 +93,38 @@ export const Auth = () => {
   return <div></div>;
 };
 >>>>>>> common elements added
+=======
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+const { Provider, Consumer: AuthConsumer } = React.createContext({
+  isAuthorized: false
+});
+
+class Auth extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isAuthorized: false
+    };
+  }
+
+  authorize = () => {
+    this.setState({ isAuthorized: true }, () => {
+      this.props.history.push("/dashboard");
+    });
+  };
+
+  render() {
+    const { isAuthorized } = this.state;
+
+    return (
+      <Provider value={{ isAuthorized, authorize: this.authorize }}>
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
+
+export default withRouter(Auth);
+>>>>>>> Authorization added
