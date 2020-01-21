cc41ad8ee100f13966dc2018e804f86e3fa9bfda
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-21 16:27:20 +0200

    Event page added

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,36 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-import PropTypes from "prop-types";
-
-export const Button = (props, { buttonState }) => {
-  return (
-    <button
-      className={props.className}
-      type={props.type}
-      disabled={!buttonState}
-    >
-      {props.label}
-    </button>
-  );
-};
-
-Button.defaultProps = {
-  type: "button",
-  className: ""
-};
-
-Button.propTypes = {
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(["button", "submit"]),
-  className: PropTypes.string
-};
-=======
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import "./SignUpPage.scss";
+import "./SignUpPage.css";
 
 import { AuthSignUp } from "../../components/auth/AuthSignUp";
 
@@ -42,8 +13,8 @@ export default class SignUpPage extends Component {
           <div className="main-back">
             <Link to="/signin">&larr;</Link>
           </div>
-          <div className="main-logo-page">
-            <i className="icon icon-mountains"></i>
+          <div className="main-logo">
+            <i className="icon icon-mountain"></i>
           </div>
           <h1 className="main__title">Sign Up</h1>
           <AuthSignUp />
@@ -52,4 +23,3 @@ export default class SignUpPage extends Component {
     );
   }
 }
->>>>>>> Authorization added
--- src/pages/eventsPage/EventsPage.jsx
+++ src/pages/eventsPage/EventsPage.jsx
@@ -1,48 +1,72 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const Input = props => {
-  const InputProps = {
-    className: props.className,
-    name: props.name,
-    type: props.type,
-    placeholder: props.placeholder,
-    value: props.value,
-    handleChange: props.handleChange,
-    label: props.label,
-    error: props.error
-  };
+export default function EventsPage() {
+  return <div></div>;
+}
 
-  return (
-    <React.Fragment>
-      {/* <label htmlFor={props.name} className="input-label">
-        {InputProps.label}
-      </label> */}
-      <input
-        className={InputProps.className}
-        name={InputProps.name}
-        type={InputProps.type}
-        value={InputProps.value}
-        handleChange={InputProps.handleChange}
-        placeholder={InputProps.placeholder}
-        style={InputProps.error && { border: "1px solid red" }}
-      />
-      {InputProps.error && <p>{InputProps.error}</p>}
-    </React.Fragment>
-  );
-};
+// export const Input = props => {
+//   const InputProps = {
+//     className: props.className,
+//     name: props.name,
+//     type: props.type,
+//     placeholder: props.placeholder,
+//     value: props.value,
+//     handleChange: props.handleChange,
+//     label: props.label,
+//     error: props.error
+//   };
 
-Input.defaultProps = {
-  type: "text",
-  className: ""
-};
+//   export class Input extends Component {
+//     constructor(props) {
+//       super(props);
 
-Input.propTypes = {
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string,
-  type: PropTypes.oneOf(["text", "number", "password"]),
-  placeholder: PropTypes.string.isRequired,
-  className: PropTypes.string,
-  value: PropTypes.any,
-  onChange: PropTypes.func.isRequired
-};
+//       this.InputProps = {
+//         className: props.settings.className,
+//         name: props.settings.name,
+//         type: props.settings.type,
+//         placeholder: props.settings.placeholder,
+//         value: props.settings.value,
+//         label: props.settings.label,
+//         error: props.settings.error
+//       };
+//     }
+
+//     render() {
+//       return (
+//         <React.Fragment>
+//           <input
+//             className={this.InputProps.className}
+//             name={this.InputProps.name}
+//             type={this.InputProps.type}
+//             value={this.InputProps.value}
+//             placeholder={this.InputProps.placeholder}
+//             style={this.InputProps.error && { border: "1px solid red" }}
+//           />
+//           {this.InputProps.error && <p>{this.InputProps.error}</p>}
+//         </React.Fragment>
+//       );
+//     }
+//   }
+
+//   Input.defaultProps = {
+//     type: "text",
+//     className: ""
+//   };
+
+//   Input.propTypes = {
+//     name: PropTypes.string, //.isRequired
+//     type: PropTypes.string,
+//     type: PropTypes.oneOf(["text", "number", "password"]),
+//     placeholder: PropTypes.string, //.isRequired
+//     className: PropTypes.string,
+//     value: PropTypes.any
+//     //onChange: PropTypes.func.isRequired
+//   };
+
+//   {
+//     /* <label htmlFor={props.name} className="input-label">
+//         {InputProps.label}
+//       </label> */
+//   }
+// };
