60809263a8fe8ec63980147efe634ccaf6bacd4c
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-21 16:27:20 +0200

    Event page added

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import "./SignUpPage.scss";
+import "./SignUpPage.css";
 
 import { AuthSignUp } from "../../components/auth/AuthSignUp";
 
@@ -13,8 +13,8 @@ export default class SignUpPage extends Component {
           <div className="main-back">
             <Link to="/signin">&larr;</Link>
           </div>
-          <div className="main-logo-page">
-            <i className="icon icon-mountains"></i>
+          <div className="main-logo">
+            <i className="icon icon-mountain"></i>
           </div>
           <h1 className="main__title">Sign Up</h1>
           <AuthSignUp />
