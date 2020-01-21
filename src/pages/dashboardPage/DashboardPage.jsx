93dfb8f9cc34b17d3b5a8e1613a79f7dd17f7a82
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-21 13:43:24 +0200

    Added: Scss, layout for dashboard component, minor fixes for fakeAPI

--- src/pages/dashboardPage/DashboardPage.jsx
+++ src/pages/dashboardPage/DashboardPage.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+import "./DashboardPage.scss";
 import Dashboard from "../../components/dashboard/Dashboard";
 
 export default class DashboardPage extends Component {
   render() {
     return (
       <div>
+        <nav className="navbar">
+          <i className="icon icon-mountains main-logo" />
+          <i className="icon icon-user icon-32" />
+        </nav>
+        <main>
+          <div className="main">
+            <aside className="sidebar">
+              <ul className="sidebar__list">
+                <div>
+                  <li className="sidebar__list__item">
+                    <NavLink
+                      to={"/dashboard"}
+                      className="sidebar__list__item__link"
+                    >
+                      News
+                    </NavLink>
+                  </li>
+                  <li className="sidebar__list__item">
+                    <NavLink
+                      to={"/events"}
+                      className="sidebar__list__item__link"
+                    >
+                      Events
+                    </NavLink>
+                  </li>
+                </div>
+                <li className="sidebar__list__item">
+                  <NavLink
+                    to={"/profile"}
+                    className="sidebar__list__item__link"
+                  >
+                    Profile
+                  </NavLink>
+                </li>
+              </ul>
+            </aside>
+            <div className="main__content">{this.props.children}</div>
+          </div>
+        </main>
         <Dashboard />
       </div>
     );
