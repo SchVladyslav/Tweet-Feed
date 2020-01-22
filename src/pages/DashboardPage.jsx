fbb8949902afcc97a301bc32f4e4ce187695f4b8
Dmitry <dmitrixkovalenko@gmail.com>
2020-01-22 13:25:00 +0200

    Preloader was added.

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import NewsDashboard from "../components/news/NewsDashboard";
-import News from "../components/news/News";
+// import NewsDashboard from "../components/news/NewsDashboard";
+// import News from "../components/news/News";
 
 export default function DashboardPage() {
   return <div></div>
