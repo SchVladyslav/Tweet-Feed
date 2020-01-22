45e0695fb35f0885e2c2f22d5482135f13395000
wellshark <55592429+wellshark@users.noreply.github.com>
2020-01-22 11:27:10 +0200

    Revert "Kovalenko branch"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import _Demo from "./components/common/_Demo";
 
 function App() {
   return (
     <div className="App">
-      <_Demo/>
+
     </div >
   );
 }
