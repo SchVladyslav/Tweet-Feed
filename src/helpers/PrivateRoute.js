ca1c60f0054001e8c0c18bb36a16ef2b459fa5c0
VladShchetyna <sch.vladislav24@gmail.com>
2020-01-22 10:22:14 +0200

    Minor fixes

--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom';
 import { authService } from '../services/auth.service';
 
-export const PrivateRoute = ({ component: Component, roles, ...rest }) => (
-    <Route {...rest} render={props => {
+export const PrivateRoute = ({ component: Component, roles, ...rest }) => {
+    debugger
+    return <Route {...rest} render={props => {
         const currentUser = authService.currentUserValue;
         if (!currentUser) {
             // not logged in so redirect to login page with the return url
@@ -20,4 +21,4 @@ export const PrivateRoute = ({ component: Component, roles, ...rest }) => (
         // authorised so return component
         return <Component {...props} />
     }} />
-);
+}
