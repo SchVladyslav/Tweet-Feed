6604091218499ea227d46dfc2ba0d1ef8124f796
Dmitry <dmitrixkovalenko@gmail.com>
2020-01-23 15:10:22 +0200

    Added common components.

--- src/components/common/button/Button.jsx
+++ src/components/common/button/Button.jsx
@@ -1,52 +1,54 @@
 import React from "react";
-import "./Button.scss";
+import './Button.scss';
+import PropTypes from 'prop-types';
 
 function getClassName(classesArray, property) {
-  if (classesArray[property]) {
-    return classesArray[property];
-  } else {
-    return classesArray.default;
-  }
+    if (classesArray[property]) {
+        return classesArray[property];
+    } else {
+        return classesArray.default;
+    }
 }
 
-export function Button({
-  children,
-  className,
-  type,
-  onClick,
-  buttonColorScheme,
-  buttonSize,
-  disabled,
-  key
-}) {
-  const colorSchemes = {
-    primary: "button_primary",
-    danger: "button_danger",
-    transparent: "button_transparent",
-    light: "button_light",
-    pearl: "button_pearl",
-    default: "button_primary"
-  };
-  const buttonSizes = {
-    small: "button_small",
-    medium: "button_medium",
-    large: "button_large",
-    default: "button_medium"
-  };
-  const colorSchemeClass = getClassName(colorSchemes, buttonColorScheme);
-  const buttonSizeClass = getClassName(buttonSizes, buttonSize);
+export default function Button({children, className, type, onClick, onSubmit, buttonColorScheme, buttonSize, disabled, key, color}) {
+    const colorSchemes = {
+        primary: 'button_primary',
+        danger: 'button_danger',
+        transparent: 'button_transparent',
+        light: 'button_light',
+        pearl: 'button_pearl',
+        default: 'button_primary'
+    };
+    const buttonSizes = {
+        small: 'button_small',
+        medium: 'button_medium',
+        large: 'button_large',
+        default: 'button_medium'
+    };
+    const colorSchemeClass = getClassName(colorSchemes, buttonColorScheme);
+    const buttonSizeClass = getClassName(buttonSizes, buttonSize);
 
-  return (
-    <button
-      key={key}
-      type={type}
-      className={`button ${colorSchemeClass} ${buttonSizeClass} ${
-        className ? className : ""
-      }`}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {children}
-    </button>
-  );
+    return <button key={key}
+                   type={type}
+                   className={`button ${colorSchemeClass} ${buttonSizeClass} ${className ? className : ''}`}
+                   onClick={onClick}
+                   onSubmit={onSubmit}
+                   disabled={disabled}
+                   style={{color: color}}
+    >{children}
+    </button>;
 }
+
+Button.propTypes = {
+    key: PropTypes.string,
+    type: PropTypes.string,
+    className: PropTypes.string,
+    buttonColorScheme: PropTypes.string,
+    buttonSize: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+    onSubmit: PropTypes.func,
+    disabled: PropTypes.bool,
+    style: PropTypes.string,
+    children: PropTypes.string
+};
--- src/components/common/input/Input.jsx
+++ src/components/common/input/Input.jsx
@@ -1,28 +1,47 @@
 import React from "react";
-import "./input.scss";
+import './input.scss';
+import PropTypes from "prop-types";
 
-export function Input({
-  key,
-  className,
-  placeholder,
-  type,
-  name,
-  errorMessage,
-  onChange
-}) {
-  return (
-    <div className="input-wrap" key={key}>
-      <input
-        className={`${className ? className : ""} input${
-          errorMessage ? " input_invalid" : ""
-        }`}
-        name={name}
-        placeholder={placeholder}
-        type={type}
-        onChange={onChange}
-      />
-      <i className="icon icon-user input__icon" />
-      {errorMessage ? <p className="error-message">{errorMessage}</p> : null}
-    </div>
-  );
+function getClassName(classesArray, property) {
+    if (classesArray[property]) {
+        return classesArray[property];
+    } else {
+        return false;
+    }
 }
+
+export default function Input({key, className, placeholder, type, errorMessage, onChange, iconName}) {
+
+    const iconNames = {
+        user: 'icon-user',
+        email: 'icon-user',
+        password: 'icon-lock',
+    };
+    const iconClass = getClassName(iconNames, iconName);
+    return <div className='input' key={key}>
+        <div className="input__control">
+            <input
+                className={`${className ? className : ''} input__field${errorMessage ? ' input__field_invalid' : ''}`}
+                placeholder={placeholder}
+                type={type}
+                onChange={onChange}
+            />
+        </div>
+        {iconClass ? <i className={`icon ${iconClass} input__icon`}/>
+            : null
+        }
+        <div className="input__error">
+            {errorMessage ? <p className='error-message'>{errorMessage}</p> : null}
+        </div>
+
+    </div>
+}
+Input.propTypes = {
+    key: PropTypes.string,
+    className: PropTypes.string,
+    placeholder: PropTypes.string,
+    type: PropTypes.string,
+    onChange: PropTypes.func,
+    errorMessage: PropTypes.string,
+    iconName: PropTypes.string
+};
