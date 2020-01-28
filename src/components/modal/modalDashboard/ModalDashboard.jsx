f60661fdf906757987a511da8a6625d10b7435bd
Dmitry <dmitrixkovalenko@gmail.com>
2020-01-28 12:48:15 +0200

    Bugs were fixed in ModalDashboard component.

--- src/components/modal/modalDashboard/ModalDashboard.jsx
+++ src/components/modal/modalDashboard/ModalDashboard.jsx
@@ -2,13 +2,12 @@ import React, {Component} from 'react';
 import './ModalDashboard.scss'
 import Button from "../../common/button/Button";
 import Textarea from "../../common/textarea/Textarea";
-import {authService} from "../../../services/auth.service";
 
 class ModalDashboard extends Component {
 
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             isFormFieldsValid: {title: null, description: null}
         }
     }
@@ -17,86 +16,87 @@ class ModalDashboard extends Component {
     componentWillMount() {
         document.addEventListener('click', this.toggleVisibility);
     }
-    componentWillUnmount(){
+
+    componentWillUnmount() {
         document.removeEventListener("click", this.toggleVisibility);
     }
-    toggleVisibility = (e) =>{
+
+    toggleVisibility = (e) => {
         const modalContent = document.querySelector('.news-modal__content');
         const modalCover = document.querySelector('.news-modal__cover');
-        if ((!e.path.includes(modalContent))&&modalCover===e.target) {
+        if ((!e.path.includes(modalContent)) && modalCover === e.target) {
             this.props.toggleModalVisibility();
         }
     };
 
-    validateField = e =>{
-        console.log('validateField');
+    validateField = e => {
         const name = e.target.name;
-        console.log(name);
         const value = e.target.value;
-        if (value.length < 1){
+        if (value.length < 1) {
             this.setState({
                 ...this.state,
-                isFormFieldsValid:{
+                isFormFieldsValid: {
                     ...this.state.isFormFieldsValid,
                     [name]: false
-                    // e.target
                 }
             })
-        }
-        else {
+        } else {
             this.setState({
                 ...this.state,
-                isFormFieldsValid:{
+                isFormFieldsValid: {
                     ...this.state.isFormFieldsValid,
                     [name]: true
-                    // e.target
                 }
             })
         }
     };
 
-    isFormValid(){
-        const {title, description} = this.state.isFormFieldsValid;
-        console.log(!Boolean( title && description))
-        return Boolean( title && description)
+    isFormValid(title, description) {
+        return Boolean(title && description)
     }
 
-    isFieldInvalid(value){
-        if(value === null){
+    isFieldInvalid(value) {
+        if (value === null) {
             return false;
         }
         return !Boolean(value);
     }
 
     render() {
-        console.log(this.state.isFormFieldsValid);
-        const { title, buttonText, handleSubmit, handleModalInput, isModalOpen, toggleModalVisibility, formTitle, formDescription } = this.props;
-        console.log(this.isFormValid());
+        const {title, buttonText, handleSubmit, handleModalInput, isModalOpen, toggleModalVisibility, formTitle, formDescription} = this.props;
         return (
             <React.Fragment>
-                {isModalOpen ? (<div className="news-modal" >
+                {isModalOpen ? (<div className="news-modal">
                     <div className="news-modal__cover"/>
                     <div className="news-modal__content position-center">
-                        <button className="news-modal__button-close" type="button" onClick={toggleModalVisibility}>X</button>
+                        <button className="news-modal__button-close" type="button" onClick={toggleModalVisibility}>X
+                        </button>
                         <h2 className="news-modal__title">{title}</h2>
                         <form className="news-modal__form" onSubmit={handleSubmit}
                               noValidate="">
                             <Textarea className='mb-20 news-modal__textarea' name="title"
                                       placeholder='Title'
                                       value={formTitle}
-                                      onBlur={(e) =>  this.validateField(e)}
-                                      onChange={(e) => {handleModalInput(e); this.validateField(e)}}
-                                      isInvalid ={this.isFieldInvalid(this.state.isFormFieldsValid.title)}
+                                      onBlur={(e) => this.validateField(e)}
+                                      onChange={(e) => {
+                                          handleModalInput(e);
+                                          this.validateField(e)
+                                      }}
+                                      isInvalid={this.isFieldInvalid(this.state.isFormFieldsValid.title)}
                             />
                             <Textarea className='mb-20 news-modal__textarea'
                                       name="description" rows="5"
                                       placeholder='Description'
                                       value={formDescription}
-                                      onBlur={(e) => this.validateField(e) }
-                                      onChange={(e) => {handleModalInput(e); this.validateField(e)}}
-                                      isInvalid ={this.isFieldInvalid(this.state.isFormFieldsValid.description)}
+                                      onBlur={(e) => this.validateField(e)}
+                                      onChange={(e) => {
+                                          handleModalInput(e);
+                                          this.validateField(e)
+                                      }}
+                                      isInvalid={this.isFieldInvalid(this.state.isFormFieldsValid.description)}
                             />
-                            <Button type='submit' buttonColorScheme='blue' buttonSize='large' disabled={!this.isFormValid()}>{buttonText}</Button>
+                            <Button type='submit' buttonColorScheme='blue' buttonSize='large'
+                                    disabled={!this.isFormValid(formTitle, formDescription)}>{buttonText}</Button>
                         </form>
                     </div>
                 </div>) : null}
