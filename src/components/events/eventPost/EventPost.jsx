5697877f70bc60fa9d98aa45fb93982733f446cc
Ann0403 <ann_st04@ukr.net>
2020-02-06 14:13:29 +0200

    add styles

--- src/components/events/eventPost/EventPost.jsx
+++ src/components/events/eventPost/EventPost.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { eventService } from '../../../services/event.service';
 import { Preloader, Button } from "../../common";
 import {withRouter} from 'react-router';
+import "./EventPost.scss";
 
 class EventPost extends Component {
 
@@ -23,24 +24,28 @@ class EventPost extends Component {
     render() {
         const { event } = this.state;
         return (
+            <>
+            <h2 className="event-page__title">Event Page</h2>
             <div className="event-page">
-                <h2>Event Page</h2>
                 {event ?
                     <>
+                    <div className="event-page__info">
                         <h3 className="events-item__title">{event.name}</h3>
                         <p className="events-item__text">Date: {event.data}</p>
                         {event.isFullDayEvent ?
-                            <strong>FULL DAY EVENT</strong>
+                            <strong style={{color: "red"}}>FULL DAY EVENT</strong>
                             :
                             <>
                                 <p className="events-item__text">Start Time: {event.startTime}</p>
                                 <p className="events-item__text">End Time: {event.endTime}</p>
                             </>
                         }
+                        </div>
                         <div className="event-page__btns">
                             <Button
                                 buttonColorScheme="primary"
                                 buttonSize="small"
+                                className="btn-control"
                             >
                                 Edit event
                             </Button>
@@ -48,6 +53,7 @@ class EventPost extends Component {
                                 buttonColorScheme="danger"
                                 buttonSize="small"
                                 onClick={() => this.onDeleteEventHandler(event.id)}
+                                className="btn-control"
                             >
                                 Delete event
                             </Button>
@@ -55,6 +61,7 @@ class EventPost extends Component {
                     </>
                     : <Preloader />}
             </div>
+            </>
         )
     }
 }
--- src/components/events/eventsItem.jsx/eventsItem.jsx
+++ src/components/events/eventsItem.jsx/eventsItem.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import './eventsItem.scss';
-import {Button} from '../../common';
+import { Button } from '../../common';
 import { Redirect } from 'react-router';
 import { NavLink } from 'react-router-dom';
 
 const EventsItem = props => {
-    const {event} = props;
-    return(
+    const { event } = props;
+    return (
         <li className="events-item">
-        <h3 className="events-item__title">{event.name}</h3>
-        <p className="events-item__text">Date: {event.date}</p>
-        {event.isFullDayEvent ? 
-        <strong>FULL DAY EVENT</strong>
-        :
-        <>
-        <p className="events-item__text">Start Time: {event.startTime}</p>
-        <p className="events-item__text">End Time: {event.endTime}</p>
-        </>
-        }
-        <NavLink to={`/event/${event.id}`}>
-        <Button
-        buttonColorScheme="success"
-        buttonSize="small"
-        >View event</Button>
-        </NavLink>
-        </li>
+            <h3 className="events-item__title">{event.name}</h3>
+            <p className="events-item__text">Date: {event.date}</p>
+            {event.isFullDayEvent ?
+                <strong style={{color: "red"}}>FULL DAY EVENT</strong>
+                :
+                <>
+                    <p className="events-item__text">Start Time: {event.startTime}</p>
+                    <p className="events-item__text">End Time: {event.endTime}</p>
+                </>
+            }
+            <NavLink to={`/event/${event.id}`}>
+                <div className="events__btn-view">
+                    <Button
+                        buttonColorScheme="success"
+                        buttonSize="small"
+                    >View event
+        </Button>
+                </div>
+            </NavLink>
+        </li >
     )
 };
 
