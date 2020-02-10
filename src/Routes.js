fcd4ff02cd8c65a5228d529d2b1eae4fb2c00692
VladShchetyna <sch.vladislav24@gmail.com>
2020-02-10 10:49:43 +0200

    PrivateRoute was fixed

--- src/Routes.js
+++ src/Routes.js
@@ -1,27 +1,26 @@
 import React from "react";
-import {Switch, Route, Router, Redirect} from "react-router";
-import {SignInPage, SignUpPage, PostPage, ProfilePage, EventPage, EventsPage, DashboardPage} from './pages/index';
-import {PrivateRoute} from "./helpers/PrivateRoute";
-import {Role} from "./helpers/FakeAPI/Role";
-import {history} from './helpers/FakeAPI/History';
+import { Switch, Route, Router, Redirect } from "react-router";
+import { SignInPage, SignUpPage, PostPage, ProfilePage, EventPage, EventsPage, DashboardPage } from './pages/index';
+import { PrivateRoute } from "./helpers/PrivateRoute";
+import { history } from './helpers/FakeAPI/History';
 
 export default function Routes() {
     return (
         <Router history={history}>
             <Switch>
-                <Route path={'/signin'} component={SignInPage}/>
-                <Route path={'/signup'} component={SignUpPage}/>
-                <Route path={'/post/:id'} component={PostPage}/>
-                <Route path={'/events'} component={EventsPage}/>
-                <Route path={'/event/:id'} component={EventPage}/>
-                <Route exact path={'/profile'} component={ProfilePage}/>
-                <PrivateRoute exact path="/dashboard" roles={[Role.User, Role.Admin]} component={DashboardPage}/>
-                <Redirect to={'signin'}/>
+                <Route path={'/signin'} component={SignInPage} />
+                <Route path={'/signup'} component={SignUpPage} />
+                <PrivateRoute path={'/post/:id'} component={PostPage} />
+                <PrivateRoute path={'/events'} component={EventsPage} />
+                <PrivateRoute path={'/event/:id'} component={EventPage} />
+                <PrivateRoute exact path={'/profile'} component={ProfilePage} />
+                <PrivateRoute exact path="/dashboard" component={DashboardPage} />
+                <Redirect to={'signin'} />
                 <Route
                     exact path={'**'}
                     component={() =>
                         <h1
-                            style={{color: 'red', textAlign: 'center', marginTop: '50px'}}>
+                            style={{ color: 'red', textAlign: 'center', marginTop: '50px' }}>
                             Page was not found!
                         </h1>}
                 />
--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -2,18 +2,17 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom';
 import { authService } from '../services/auth.service';
 
-export const PrivateRoute = ({ component: Component, roles, ...rest }) => {
+export const PrivateRoute = ({ component: Component, ...rest }) => {
     return <Route {...rest} render={props => {
         const currentUser = authService.currentUser;
         if (!currentUser) {
-            console.log(authService.currentUser);
             return <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
         }
 
         // check if route is restricted by role
-        if (roles && roles.indexOf(currentUser.role) === -1) {
-            return <Redirect to={{ pathname: '/signin' }} />
-        }
+        // if (roles && roles.indexOf(currentUser.role) === -1) {
+        //     return <Redirect to={{ pathname: '/signin' }} />
+        // }
 
         return <Component {...props} />
     }} />
